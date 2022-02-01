ba91b4d1631946e5504756d299782a6f5c73bba4
Maël CHAUMONT <machaumont2@IUTCLINFA2319.iut.local>
2022-02-01 15:27:16 +0100

    premiers ajouts

--- code/app/src/main/java/com/example/myapplication/MainActivityAccueil.java
+++ code/app/src/main/java/com/example/myapplication/MainActivityAccueil.java
@@ -1,17 +1,21 @@
 package com.example.myapplication;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
-public class MainActivity extends Activity {
+public class MainActivityAccueil extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.viewaccueil);
+    }
+
+    public void openViewJeu(View view) {
+        Intent monIntent = new Intent(this, ActivityJeu.class);
+        startActivity(monIntent);
     }
 }
